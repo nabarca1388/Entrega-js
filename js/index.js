a112645e5274a06ec421d5aec376ec6c98e07644
nabarca1388 <nicolas.m.abarca@gmail.com>
2022-12-13 19:56:25 -0300

    segunda entrega

---
+++ js/index.js
@@ -0,0 +1,129 @@
+function resta(valorCompra, valorMercado){
+    return valorMercado - valorCompra;
+}
+
+function variacionDiaria(apertura, cierre){
+    return (apertura - cierre)/10
+}
+
+function precioCompra(apertura, cierre){
+    return (apertura + cierre)/2
+}
+
+function interesFCI(dinero, interes){
+    return ((interes * dinero) / 100) + dinero
+}
+
+//ACCIONES---------------------------------------------------------------------------------------------
+
+class Accion{
+    constructor(nombre, prApertura, prCierre, prCompra, variacion){
+        this.nombre = nombre;
+        this.prApertura = prApertura;
+        this.prCierre = prCierre;
+        this.prCompra = prCompra;
+        this.variacion = variacion;
+    }
+}
+
+//BONOS--------------------------------------------------------------------------------------------------
+class PlazoFijo{
+    constructor(cantidad, dias, interes){
+        this.cantidad = cantidad;
+        this.dias = dias;
+        this.interes = interes;
+    }
+}
+
+//BILLETERA----------------------------------------------------------------------
+class Billetera{
+    constructor(activo, prCompra, prDia, ganancia){
+        this.activo = activo;
+        this.prCompra = prCompra;
+        this.prDia = prDia;
+        this.ganancia = ganancia;
+    }
+}
+
+
+//Creo objetos de acciones en array
+const accionesMercado = [];
+accionesMercado.push(new Accion(
+    "ggal",
+     980, 
+     1000,  
+))
+accionesMercado.push(new Accion(
+    "tesla",
+     2300, 
+     2240,  
+))
+
+
+accionesMercado.forEach(item =>{
+    item.variacion = variacionDiaria(item.prApertura, item.prCierre);
+    item.prCompra = precioCompra(item.prApertura, item.prCierre);
+})
+
+/*
+accionesMercado.forEach(item =>{
+    console.log(`Nombre: ${item.nombre}`);
+    console.log(`Precio apertura: $${item.prApertura}`);
+    console.log(`Precio cierre: $${item.prCierre}`);
+    console.log(`Precio compra: $${item.prCompra}`);
+    console.log(`Variacion: %${item.variacion}`);
+})
+*/
+
+//Creo objetos de plazo fijo
+let dineroFCI = 100000;
+
+const fci = [];
+
+fci.push(new PlazoFijo(
+    dineroFCI,
+    30,
+    4.5,
+))
+
+fci.push(new PlazoFijo(
+    dineroFCI,
+    60,
+    10.2,
+))
+
+fci.push(new PlazoFijo(
+    dineroFCI,
+    90,
+    18,
+))
+
+fci.forEach(item =>{
+    item.dineroFCI = interesFCI(item.cantidad, item.interes);
+})
+
+
+//Cargo mi billetera con objetos
+const miBilletera = [];
+
+let valor = accionesMercado.length + fci.length;
+let x = 0;
+
+for(let i = 0; i < valor; i++){
+    
+    if(i < fci.length){
+        miBilletera[i] = fci[i];
+    }else{
+        miBilletera[i] = accionesMercado[x];
+        x++;
+    }
+}
+
+
+console.log(`TUS ACTIVOS SON: `, miBilletera);
+
+
+
+
+
+
